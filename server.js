c76c7da953832e800f07fdca20b8ac80564b35e4
Rajasekaran Radhakrishnan <rrajsekarm@gmail.com>
2016-12-29 15:16:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Welcome to Blue Green deployment steps!\n')
+  response.end('Welcome to Blue Green deployment !!\n')
 })
 
 server.listen(port)
